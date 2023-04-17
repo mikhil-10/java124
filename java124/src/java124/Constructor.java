f2fc74c94f9771c9f101026a9e3e5d205c465369
Dattatray <Dattatray@Mikhil>
2023-04-17 10:59:15 +0530

    Constructor

--- java124/src/java124/Constructor.java
+++ java124/src/java124/Constructor.java
@@ -4,13 +4,14 @@ public class Constructor {
 	int id;
 	String name;
 
-	//go in Source and select Generate Constructor using field 
-	
+	// go in Source and select Generate Constructor using field
+
 	public Constructor(int id, String name) {
 		super();
 		this.id = id;
 		this.name = name;
 	}
+
 // now again go in source and select generate toString() This will convert hashcode() into string
 	@Override
 	public String toString() {
@@ -18,7 +19,7 @@ public class Constructor {
 	}
 
 	public static void main(String[] args) {
-		Constructor ob = new Constructor(1124, "Mikhil");
+		Constructor ob = new Constructor(1124, " Mikhil");
 		System.out.println(ob.toString());
 	}
 }
