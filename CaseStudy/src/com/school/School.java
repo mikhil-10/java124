25dd38b917a82714af3236114babd48ee25d1c29
MIKHIL <MIKHIL@LAPTOP-MDE3C5U2>
2023-04-30 11:41:36 +0530

    Case Study of School

---
+++ CaseStudy/src/com/school/School.java
@@ -0,0 +1,68 @@
+package com.school;
+
+class subject {
+	int sub(int chapter, int covered) {
+		System.out.println("remaining chapters ");
+		return chapter - covered;
+	}
+
+	int sub(int chapter) {
+		System.out.println("Total chapters");
+		return chapter;
+	}
+}
+
+class student extends subject {
+	String name;
+	int id;
+	public student(String name, int id) {
+		super();
+		this.name = name;
+		this.id = id;
+		System.out.println("Student name and id : ");
+	}
+
+	@Override
+	int sub(int chapter) {
+		// TODO Auto-generated method stub
+		return super.sub(chapter);
+	}
+
+	@Override
+	public String toString() {
+		return "student [name=" + name + ", id=" + id + "]";
+	}
+}
+
+class teacher extends subject {
+	String name;
+	int id;
+
+	public teacher(String name, int id) {
+		super();
+		this.name = name;
+		this.id = id;
+		System.out.println("Teacher name and id: ");
+	}
+	@Override
+	int sub(int chapter, int covered) {
+		// TODO Auto-generated method stub
+		return super.sub(chapter, covered);
+	}
+	@Override
+	public String toString() {
+		return "teacher [name=" + name + ", id=" + id + "]";
+	}
+}
+public class School {
+	public static void main(String[] args) {
+		student s = new student("Student1 ",24);
+		System.out.println(s);
+		System.out.println(s.sub(20));
+		teacher t = new teacher("Teacher1",101);
+		System.out.println(t);
+		System.out.println(t.sub(20, 3));
+		
+		
+	}
+}
